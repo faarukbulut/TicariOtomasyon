7a53a7a5c43781b6cce05a99fb0c7b0ccaa4c42b
faarukbulut <faarukbulut@gmail.com>
2023-12-05 18:10:14 +0300

    Cari ve Personel Sınıfı

---
+++ Models/Siniflar/Cari.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Cari
+	{
+		[Key]
+		public int CariID { get; set; }
+		public string CariAd { get; set; }
+		public string CariSoyad { get; set; }
+		public string CariSehir { get; set; }
+		public string CariMail { get; set; }
+	}
+}
---
+++ Models/Siniflar/Personel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Personel
+	{
+		[Key]
+		public int PersonelID { get; set; }
+		public string PersonelAd { get; set; }
+		public string PersonelSoyad { get; set; }
+		public string PersonelGorsel { get; set; }
+	}
+}
