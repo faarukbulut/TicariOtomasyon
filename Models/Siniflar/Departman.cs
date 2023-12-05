272488a12d12c24406c7a7eb4fc7f852bb7f43e3
faarukbulut <faarukbulut@gmail.com>
2023-12-05 18:14:33 +0300

    Fatura ve Departman Sınıfı

---
+++ Models/Siniflar/Departman.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Departman
+	{
+		[Key]
+		public int DepartmanID { get; set; }
+		public string DepartmanAd { get; set; }
+	}
+}
---
+++ Models/Siniflar/Fatura.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Fatura
+	{
+		[Key]
+		public int FaturaID { get; set; }
+		public string FaturaSeriNo { get; set; }
+		public string FaturaSiraNo { get; set; }
+		public DateTime Tarih { get; set; }
+		public string VergiDairesi { get; set; }
+		public DateTime Saat { get; set; }
+		public string TeslimEden { get; set; }
+		public string TeslimAlan { get; set; }
+	}
+}
