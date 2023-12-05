112e7c6a01b97b26279215d8569f63d28a3bfce6
faarukbulut <faarukbulut@gmail.com>
2023-12-05 18:07:21 +0300

    Kategori ve Ürün Sınıfı

---
+++ Models/Siniflar/Kategori.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Kategori
+	{
+		[Key]
+		public int KategoriID { get; set; }
+		public string KategoriAd { get; set; }
+	}
+}
---
+++ Models/Siniflar/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class Urun
+	{
+		[Key]
+		public int UrunID { get; set; }
+		public string UrunAd { get; set; }
+		public string Marka { get; set; }
+		public short Stok { get; set; }
+		public decimal AlisFiyat { get; set; }
+		public decimal SatisFiyat { get; set; }
+		public bool Durum { get; set; }
+		public string UrunGorsel { get; set; }
+	}
+}
