855042f86006db4ca7fd5be49c3f93c9bc1b159e
faarukbulut <faarukbulut@gmail.com>
2023-12-05 18:21:11 +0300

    Satış Hareketleri Sınıfı

---
+++ Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicariOtomasyon.Models.Siniflar
+{
+	public class SatisHareket
+	{
+		[Key]
+		public int SatisHareketID { get; set; }
+		// Ürün
+		// Cari
+		// Personel
+		public DateTime Tarih { get; set; }
+		public int Adet { get; set; }
+		public decimal Fiyat { get; set; }
+		public decimal ToplamTutar { get; set; }
+	}
+}
