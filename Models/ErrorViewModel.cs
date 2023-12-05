471100c79d108ef0f18876f1866b64bec58fe577
faarukbulut <faarukbulut@gmail.com>
2023-12-05 18:00:58 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicariOtomasyon.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
